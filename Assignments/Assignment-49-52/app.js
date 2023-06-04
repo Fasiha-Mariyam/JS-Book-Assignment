a86cd1d4e5bf8d471c98ca8cb84824d11866d261
Fasiha-Mariyam <125477695+Fasiha-Mariyam@users.noreply.github.com>
2023-06-04 19:11:36 +0500

    Add files via upload

--- Assignments/Assignment-49-52/app.js
+++ Assignments/Assignment-49-52/app.js
@@ -26,3 +26,83 @@
 // an edit button. On click on delete button entire row should
 // be deleted. On click on edit button, a hidden form will
 // appear with the values of that row.
+function addStudent(event){
+    event.preventDefault();
+ var studentName = document.getElementById('name').value;
+ console.log(studentName);
+ var studentAge = document.getElementById('age').value;
+ var row = document.createElement('tr');
+ var nameData = document.createElement('td');
+ var ageData = document.createElement('td');
+ var actionData = document.createElement('td');
+nameData.textContent = studentName;
+ageData.textContent = studentAge;
+ 
+var deleteButton = document.createElement('button');
+deleteButton.textContent = 'delete';
+deleteButton.addEventListener('click',removeRow);
+
+var editButton = document.createElement('button');
+editButton.textContent = 'edit';
+editButton.addEventListener('click',editRow)
+
+actionData.appendChild(editButton);
+actionData.appendChild(deleteButton);
+
+row.appendChild(nameData);
+row.appendChild(ageData);
+row.appendChild(actionData);
+
+var studentTable = document.getElementById("studentTable");
+studentTable.appendChild(row);
+}
+function removeRow(event){
+    const row = event.target.parentNode.parentNode;
+    // console.log(row.parentNode.removeChild(row));
+            row.parentNode.removeChild(row);
+}
+function cancelEdit(){
+
+    editForm.classList.add("hidden");
+
+}
+function editRow(event){
+    var row = event.target.parentNode.parentNode;
+    console.log(row);
+    
+    var nameCell = row.getElementsByTagName('td')[0];
+    var ageCell = row.getElementsByTagName('td')[1];
+
+    var editName = document.getElementById('editname');
+    var editAge = document.getElementById('editage');
+
+    editName.value = nameCell.textContent;
+    editAge.value = ageCell.textContent;
+
+    editForm.classList.remove('hidden');
+    editForm.dataset.rowIndex = row.rowIndex;
+//It sets the data in the attribute and reads this data using JavaScript events.
+}
+function saveEdit(event){
+    event.preventDefault();
+    var index = editForm.dataset.rowIndex;
+    console.log(index);
+    var row = studentTable.rows[index];
+    console.log(row);
+    var nameCell = row.getElementsByTagName('td')[0];
+    var ageCell = row.getElementsByTagName('td')[1];
+
+    var editName = document.getElementById('editname').value;
+    var editAge = document.getElementById('editage').value;
+
+    nameCell.textContent = editName;
+    ageCell.textContent =   editAge ;
+    editForm.classList.add('hidden');
+    //return CSS classes
+}
+
+
+
+
+
+
--- Assignments/Assignment-53-57/app.js
+++ Assignments/Assignment-53-57/app.js
@@ -1,1 +1,16 @@
-alert("hello")
+// 2. Create a paragraph and two buttons “zoom in ”(+) and “zoom out”(-).
+// On each click on “zoom in”(+) , add 10px in font size of paragraph.
+// And on each click on “zoom out”(-) , minus 10px in font size of paragraph
+function zoomin(){
+var paragraph = document.getElementById('zoom');
+var fontSize = parseInt(window.getComputedStyle(paragraph).fontSize);
+var pro = fontSize+10+"px";
+paragraph.style.fontSize = pro;
+}
+function zoomout(){
+    var paragraph = document.getElementById('zoom');
+    var fontSize = parseInt(window.getComputedStyle(paragraph).fontSize);
+    var pro = fontSize-10+"px";
+    paragraph.style.fontSize = pro;
+}
+
